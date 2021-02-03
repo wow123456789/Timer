55aef38890d63a3555e4b8a7469acaeac693cc49
wow123456789 <68571872+wow123456789@users.noreply.github.com>
2021-02-03 17:46:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,6 @@ greeting = document.getElementById('greeting'),
   name = document.getElementById('name'),
   focus = document.getElementById('focus');
 
-// Options
-const showAmPm = true;
-
-  // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
-
-  // 12hr Format
-  hour = hour % 12 || 12;
-
-// Add Zeros
-function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
-}
 
 // Set Background and Greeting
 function setBgGreet() {
